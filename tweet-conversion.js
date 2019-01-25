82cbc49bbc0ff0f24bc1b31efe00d9424a848994
Garrett Hogan <garrett.hogan.dev@gmail.com>
2019-01-25 00:39:35 -0800

    trying to figure out this matrix math

--- tweet-conversion.js
+++ tweet-conversion.js
@@ -19,6 +19,37 @@ const createEmojiWorld = emojiCount => {
   return map;
 };
 
+/**
+ *
+ * TODO: Figure out how to properly map from flat array to matrix:
+ *
+ * [0, 0, 0, 0]
+ * [0, 0, 0, 0]
+ * [0, 0, 0, 0]
+ * [0, 0, 0, 0]
+ *
+ * To get last element in each row is (numRows * (currentRowIndex + 1)) - 1
+ *
+ * if currentRowIndex is 0
+ * - (4 x (0 + 1)) - 1 = 3 = flatArray[3] === last element in the first row
+ *
+ * if currentRowIndex is 1
+ * - (4 x (1 + 1)) - 1 = 7 = flatArray[7] === last element in the second row
+ *
+ * To get an arbitrary element you need the column offset which is:
+ * ((numRows * (currentRowIndex + 1)) - 1) - columnOffsetIndex
+ *
+ * if we want to map the second element in the array to the appropriate spot:
+ *
+ * if currentRowIndex = 1 and currentColumnIndex = 1, that should correspond to flatArray[5]
+ * (4 x (1 + 1)) - 1 = 7 this gives us the last element of that row
+ * then just subtract the currentColumnIndex + 1 and you get 5.
+ *
+ * if currentRowIndex = 2 and currentColumnIndex = 2, that should correspond to flatArray[11]
+ * (4 x (2 + 1)) - 1 = 11 here we get the last item of the row
+ *
+ */
+
 const mapToMatrix = (tweet = createEmojiWorld(140)) => {
   emojis = emoji
     .unemojify(tweet)
